9ed5b3264d4b837a456ef06b7b9fc7d6cfed2845
AronBrown <44118851+AronBrown@users.noreply.github.com>
2021-01-13 21:32:25 +0100

    Cambiato progetto del tutto

---
+++ ArrayDante.js
@@ -0,0 +1,4 @@
+var versi = new Array("1 Nel mezzo del cammin di nostra vita \n 2 mi ritrovai per una selva oscura, 3 ché la diritta via era smarrita.",
+					"4 Ahi quanto a dir qual era è cosa dura 5 esta selva selvaggia e aspra e forte 6 che nel pensier rinova la paura!",
+					"7 Tant’è amara che poco è più morte; 8 ma per trattar del ben ch’i’ vi trovai, 9 dirò de l’altre cose ch’i’ v’ ho scorte.",
+					"10 Io non so ben ridir com’i’ v’intrai, 11 tant’era pien di sonno a quel punto 12 che la verace via abbandonai.")
