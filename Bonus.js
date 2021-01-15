948aaf43e3856a011097eabd4c021142686452ab
AronBrown <44118851+AronBrown@users.noreply.github.com>
2021-01-15 17:08:33 +0100

    180 assurdo forse ci metto anche l'icona chissà

--- Bonus.js
+++ Bonus.js
@@ -1,4 +1,6 @@
-var versiBonus = new Array("A volte mi rendo conto che esagero\nnon so gestire i miei sentimenti e men che mai la mia rabbia\nvederti vestita in quel modo l'altra sera al Queenrider di Tivoli\nassieme a Giacomo che tu dicevi essere solo un amico\nmi ha fatto un effetto strano\nquella reazione lo sai non appartiene al Valerio con cui hai passato sei anni\ncon cui hai passato quel weekend a Istanbul\nquel mese in Giappone nel quale ci siamo promessi amore eterno\ncosa ne è ora di quel tempo passato assieme\nio non ti ho mai sfiorato con un fiore Letizia\nmai in cinque anni\nè stato orribile\nil tuo sguardo\nquel mio gesto\nil dover discutere con un agente di polizia fino alle tre del mattino\ntu ora mi vedi che faccio il clown qui in televisione\nma qua sotto c'è un cuore distrutto per te\nchiamami appena puoi");
+var versiBonus = new Array("Però c’è un’altra cosa che voglio dirti\nche credo sia il vero,\ngrande merito di questa fiction:\nè che non ci sono i toscani, capisci?\nPerché con quella c aspirata\ne quel senso dell’umorismo da quattro soldi\ni toscani hanno devastato questo Paese.",
+	"Devi stare muto, basso profilo,\nchiappe strette e sperare in Dio."
+	);
 
 $(document).ready(function(){
 	$("#button4").click(function(){
